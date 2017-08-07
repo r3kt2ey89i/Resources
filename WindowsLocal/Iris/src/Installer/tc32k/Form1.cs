99ae0040512b7f22088cd10dc710feb1b1901a6f
Yn <30512781+akayn@users.noreply.github.com>
2017-08-07 04:41:25 +0300

    Add files via upload

---
+++ WindowsLocal/Iris/src/Installer/tc32k/Form1.cs
@@ -0,0 +1,31 @@
+
+/*
+	GNU AFFERO GENERAL PUBLIC LICENSE
+        Version 3, 19 November 2007
+	Copyrights © Numéro du projet sept sérine.
+	author: sérine
+*/
+
+// we use a form application to
+// hide the existance of the program.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackDoor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
